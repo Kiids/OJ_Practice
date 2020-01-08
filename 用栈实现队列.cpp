5db03c2eb6886fc173e9d5af64fb1bb1438e5ea8
Kiids <1980774293@qq.com>
2020-01-08 19:59:57 +0800

    使用栈实现队列的下列操作

---
+++ 用栈实现队列.cpp
@@ -0,0 +1,61 @@
+// 使用栈实现队列的下列操作：
+// push(x) -- 将一个元素放入队列的尾部。
+// pop() -- 从队列首部移除元素。
+// peek() -- 返回队列首部的元素。
+// empty() -- 返回队列是否为空。
+
+#include <stack>
+
+class MyQueue {
+    stack<int> s1;
+    stack<int> s2;
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (s2.empty())
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        int top = s2.top();
+        s2.pop();
+        return top;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        if (s2.empty())
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        int top = s2.top();
+        return top;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
