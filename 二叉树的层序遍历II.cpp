574a89f4cad56090ad8c4390308cbea90ce09313
Kiids <1980774293@qq.com>
2020-01-12 00:24:51 +0800

    给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）

---
+++ 二叉树的层序遍历II.cpp
@@ -0,0 +1,59 @@
+/*
+给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其自底向上的层次遍历为：
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ret;
+		if (root == NULL)
+			return ret;
+
+		vector<int> v;
+        
+		queue<TreeNode*> q;
+		TreeNode* cur = root;
+		q.push(cur);
+		int len = 1;  // 控制每层个数
+		while (!q.empty())
+		{
+			for (int i = 0; i < len; ++i)
+			{
+				cur = q.front();
+				v.push_back(cur->val);
+				q.pop();
+				if (cur->left)
+					q.push(cur->left);
+				if (cur->right)
+					q.push(cur->right);
+			}
+			ret.push_back(v);
+			v.clear();
+			len = q.size();
+		}
+        reverse(ret.begin(), ret.end());
+		return ret;
+    }
+};
