92ffd8136e5034ea0e67a415a0f91439f85ac790
Kiids <1980774293@qq.com>
2020-07-10 01:51:46 +0800

    栈排序。 编写程序，对栈进行排序使最小元素位于栈顶。最多只能使用一个其他的临时栈存放数据，但不得将元素复制到别的数据结构（如数组）中。该栈支持如下操作：push、pop、peek 和 isEmpty。当栈为空时，peek?返回 -1。

---
+++ 栈排序.cpp
@@ -0,0 +1,69 @@
+/*
+栈排序。 编写程序，对栈进行排序使最小元素位于栈顶。最多只能使用一个其他的临时栈存放数据，但不得将元素复制到别的数据结构（如数组）中。该栈支持如下操作：push、pop、peek 和 isEmpty。当栈为空时，peek?返回 -1。
+示例1:
+ 输入：
+["SortedStack", "push", "push", "peek", "pop", "peek"]
+[[], [1], [2], [], [], []]
+ 输出：
+[null,null,null,1,null,2]
+示例2:
+ 输入： 
+["SortedStack", "pop", "pop", "push", "pop", "isEmpty"]
+[[], [], [], [1], [], []]
+ 输出：
+[null,null,null,null,null,true]
+说明:
+栈中的元素数目在[0, 5000]范围内。
+*/
+
+class SortedStack {
+public:
+    stack<int> s1, s2;
+    SortedStack()
+	{
+        
+    }
+    
+    void push(int val)
+	{
+        while (!s1.empty() && s1.top() < val)
+		{
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(val);
+        while (!s2.empty())
+		{
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+    
+    void pop()
+	{
+        if(!s1.empty())
+            s1.pop();
+    }
+    
+    int peek()
+	{
+        if(!s1.empty())
+            return s1.top();
+        return -1;
+    }
+    
+    bool isEmpty()
+	{
+        return s1.empty();
+    }
+
+};
+
+/**
+ * Your SortedStack object will be instantiated and called as such:
+ * SortedStack* obj = new SortedStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->isEmpty();
+ */
