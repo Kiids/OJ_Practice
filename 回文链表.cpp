606ed5551a8746bccde72b0babd2bdbc9b52b5b5
Kiids <1980774293@qq.com>
2020-12-31 15:42:24 +0800

    请判断一个链表是否为回文链表。

--- 回文链表.cpp
+++ 回文链表.cpp
@@ -56,3 +56,33 @@ public:
         return true;  
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> s;
+        ListNode *p = head;
+        while (p)
+        {
+            s.push(p->val);
+            p = p->next;
+        }
+        p = head;
+        while (p)
+        {
+            if (p->val != s.top())
+                return false;
+            s.pop();
+            p = p->next;
+        }
+        return true;
+    }
+};
