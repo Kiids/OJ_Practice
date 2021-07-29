4bb50455583b1bcb59fb801e8a82a31cc3454c74
Kiids <1980774293@qq.com>
2021-07-30 00:23:28 +0800

    给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 进阶：你能尝试使用一趟扫描实现吗？

---
+++ 删除链表的倒数第 N 个结点.cpp
@@ -0,0 +1,48 @@
+/*
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+进阶：你能尝试使用一趟扫描实现吗？
+示例 1：
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+示例 2：
+输入：head = [1], n = 1
+输出：[]
+示例 3：
+输入：head = [1,2], n = 1
+输出：[1]
+提示：
+链表中结点的数目为 sz
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* x = new ListNode(0, head);
+        ListNode* q = x;
+        int l = 0;
+        while (head)
+        {
+            l++;
+            head = head->next;  
+        }
+        for (int i = 0; i < l - n; i++)
+            q = q->next;
+        q->next = q->next->next;
+        ListNode* head2 = x->next;
+            delete x;
+        return head2;
+    }
+};
