48d5188139f03b21077217d1dc260415290f4a65
Kiids <1980774293@qq.com>
2021-08-09 00:02:56 +0800

    有一个二维矩阵 A 其中每个元素的值为 0 或 1 。 移动是指选择任一行或列，并转换该行或列中的每一个值：将所有 0 都更改为 1，将所有 1 都更改为 0。 在做出任意次数的移动后，将该矩阵的每一行都按照二进制数来解释，矩阵的得分就是这些数字的总和。 返回尽可能高的分数。

---
+++ 翻转矩阵后的得分.cpp
@@ -0,0 +1,43 @@
+/*
+有一个二维矩阵 A 其中每个元素的值为 0 或 1 。
+移动是指选择任一行或列，并转换该行或列中的每一个值：将所有 0 都更改为 1，将所有 1 都更改为 0。
+在做出任意次数的移动后，将该矩阵的每一行都按照二进制数来解释，矩阵的得分就是这些数字的总和。
+返回尽可能高的分数。
+示例：
+输入：[[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+输出：39
+解释：
+转换为 [[1,1,1,1],[1,0,0,1],[1,1,1,1]]
+0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+提示：
+1 <= A.length <= 20
+1 <= A[0].length <= 20
+A[i][j] 是 0 或 1
+*/
+
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int n0 = 0, n1 = 0, ret = 0;
+        int n = grid.size(), m = grid[0].size();
+        if (n == 0 && m == 0)
+            return ret;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+                if (i == 0 && grid[j][i] == 0)
+                    for (int k = 0; k < m; k++)
+                        grid[j][k] = -(grid[j][k] - 1);
+                else
+                    grid[j][i] ? n1++ : n0++;
+            if (n0 > n1)
+                for (int j = 0; j < n; j++)
+                    grid[j][i] = -(grid[j][i] - 1);
+            for (int j = 0; j < n; j++)
+                if (grid[j][i])
+                    ret += pow(2, m - i - 1);
+            n0 = n1 = 0;
+        }
+        return ret;
+    }
+};
