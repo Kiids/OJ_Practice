5c409c1776bb86295f9e7036d8e60dad2586cac6
Kiids <1980774293@qq.com>
2022-02-10 00:26:54 +0800

    给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。

---
+++ 去除重复字母.cpp
@@ -0,0 +1,46 @@
+/*
+给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+示例 1：
+输入：s = "bcabc"
+输出："abc"
+示例 2：
+输入：s = "cbacdcbc"
+输出："acdb"
+提示：
+1 <= s.length <= 10^4
+s 由小写英文字母组成
+*/
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> count;
+        unordered_set<char> us;
+        stack<char> st;
+        for (int i = 0; i < s.size(); i++)
+            count[s[i]]++;
+        for (int i = 0; i < s.size(); i++)
+        {
+            char c = s[i];
+            count[c]--;
+            if (us.find(c) == us.end())
+            {
+                while (!st.empty() && st.top() > c && count[st.top()] != 0)
+                {
+                    us.erase(st.top());
+                    st.pop();
+                }
+                us.emplace(c);
+                st.push(c);
+            }
+        }
+        string ret = "";
+        while (!st.empty())
+        {
+            ret.push_back(st.top());
+            st.pop();
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
