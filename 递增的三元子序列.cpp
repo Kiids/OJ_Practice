6f5df8fde235c3fd485c3818f511802436979e04
Kiids <1980774293@qq.com>
2022-04-02 01:20:09 +0800

    递增子序列查找

--- 递增的三元子序列.cpp
+++ 递增的三元子序列.cpp
@@ -27,12 +27,26 @@ public:
         for (auto num : nums)
         {
             if (num <= small)
-            small = num;
+            	small = num;
             else if (num <= mid)
                 mid = num;
             else if (num > mid)
-            return true;
+            	return true;
         }
         return false;    
     }
 };
+
+//新建两个变量 small 和 mid ，用来保存长度为 3 的递增子序列的最小值和中间值。
+//遍历数组，每遇到一个数字，将它和 small 和 mid 相比，若小于等于 small ，则替换 small；
+//否则，若小于等于 mid，则替换 mid；否则，若大于 mid，则说明找到了长度为 3 的递增数组
+//
+//当已经找到了长度为 2 的递增序列，这时又来了一个比 small 还小的数字，为什么可以直接替换 small ，
+//这样 small 和 mid 在原数组中并不是按照索引递增的关系
+//假如当前的 small 和 mid 为 [3, 5]，这时又来了个 1。假如我们不将 small 替换为 1，
+//那么，当下一个数字是 2，后面再接上一个 3 的时候，我们就没有办法发现这个 [1,2,3] 的递增数组了
+//也就是说，替换最小值，是为了后续能够更好地更新中间值！
+//即使更新了 small ，这个 small 在 mid 后面，没有严格遵守递增顺序，但它隐含着的真相是，
+//有一个比 small 大比 mid 小的前最小值出现在 mid 之前。因此，当后续出现比 mid 大的值的时候，
+//一样可以通过当前 small 和 mid 推断的确存在着长度为 3 的递增序列
+
