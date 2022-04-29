a94a7a5aa9aad5680f69b7d96f55bd73e7709faa
Kiids <1980774293@qq.com>
2022-04-30 01:11:17 +0800

    全排列

--- 下一个更大元素 III.cpp
+++ 下一个更大元素 III.cpp
@@ -20,3 +20,10 @@ public:
         return -1;
     }
 };
+
+//atd::stoll str->long long int
+//next_permutation（start,end），和prev_permutation（start,end）。
+//这两个函数作用是一样的，区别就在于前者求的是当前排列的下一个排列，后一个求的是当前排列的上一个排列。
+//“前一个”和“后一个”，我们可以把它理解为序列的字典序的前后，
+//就是对于当前序列pn，他的下一个序列pn+1满足：不存在另外的序列pm，使pn<pm<pn+1.
+
