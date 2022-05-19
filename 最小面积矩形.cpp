30313e2a8ff02bc52175bd75069bfb8ff1d51a9b
Kiids <1980774293@qq.com>
2022-05-20 01:26:35 +0800

    对角线

--- 最小面积矩形.cpp
+++ 最小面积矩形.cpp
@@ -25,9 +25,13 @@ public:
         for (int i = 0; i < len; i++)
             for (int j = i + 1; j < len; j++)
                 if (points[i][0] != points[j][0] && points[i][1] != points[j][1])
-                    if (s.count(points[i][0]* 40001 + points[j][1]) && s.count(points[j][0] * 40001 + points[i][1]))
+                    if (s.count(points[i][0] * 40001 + points[j][1]) && s.count(points[j][0] * 40001 + points[i][1]))
                         ret = min(ret, abs(points[j][1] - points[i][1]) * abs(points[j][0] - points[i][0]));
 
         return ret < INT_MAX ? ret: 0;
     }
 };
+
+//先确定两个点作为对角线，然后去找另外两个点是否存在，存在则可以快速计算面积
+//快速查找方式: 预先按照点去构建去存储到set里，考虑到范围，用一个数字就可以保存了
+
