47529f3337374452e49360f50f40482fa36d9b40
Kiids <1980774293@qq.com>
2022-08-20 01:46:09 +0800

    计算正方形最近的点和圆心的距离是否小于等于半径

--- 圆和矩形是否有重叠.cpp
+++ 圆和矩形是否有重叠.cpp
@@ -25,8 +25,11 @@ y1 < y2
 class Solution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+    	// 包含在里面则直接设置为0
         int dx = x1 > xCenter ? (x1 - xCenter) : (x2 < xCenter ? xCenter - x2 : 0);
         int dy = y1 > yCenter ? (y1 - yCenter) : (y2 < yCenter ? yCenter - y2 : 0);
         return dx * dx + dy * dy <= radius * radius;
     }
 };
+
+// 计算正方形最近的点和圆心的距离是否小于等于半径
