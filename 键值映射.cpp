6260c45e27eb6a5643df05091b2b261db20f023b
Kiids <1980774293@qq.com>
2022-11-01 00:34:42 +0800

    map

--- 键值映射.cpp
+++ 键值映射.cpp
@@ -38,22 +38,22 @@ public:
     
     int sum(string prefix) {
         auto r = m.insert({prefix, 0});
-        auto b = r.first;
-        if (r.second)
+        auto b = r.first;  // b为第一个匹配的key对应的迭代器
+        if (r.second)      // 若以前没有（成功插入）则b后移
         {
             b++;
             m.erase(prefix);
         }
         (*(prefix.end() - 1)) ++;
         r = m.insert({prefix, 0});
-        auto e = r.first;
-        if (r.second)
+        auto e = r.first;  // e为最后一个匹配的key对应的迭代器加一
+        if (r.second)      // 若以前没有（成功插入）则e后移
         {
             e++;
             m.erase(prefix);
         }
         int ret = 0;
-        while (b != e)
+        while (b != e)     // 累加b到e-1
         {
             ret += b->second;
             b++;
@@ -68,3 +68,13 @@ public:
  * obj->insert(key,val);
  * int param_2 = obj->sum(prefix);
  */
+ 
+
+//存储和插入，直接插入map，空间复杂度空间复杂度O(M)，insert时间复杂度O(logM)，M为要存储的键值对个数。
+//求sum，因为map有序，需要累加的key在map中连续，只需要确定符合条件的key在map中的起始位置和结束位置，之后累加。
+//起始位置，插入一个prefix键值，如果插入失败，表示map原本就有prefix,map::insert函数返回的迭代器即为起始位置，
+//如果插入成功，表示map原本不存在prefix，map::insert函数返回的迭代器加上1即为起始位置。
+//结束位置，需要先将prefix字符串的最后一个字符加一（例如原本为"abc", 加一后变为"abd"）,再插入map中，
+//位置在最后一个符合条件的key后面，减1就确定最后一个符合条件key的位置。
+//sum函数的时间复杂度为O(logM + C), M为要存储的键值对个数，C为匹配到的key值个数。
+
