2e25f86780bc1e7fad33e1aca760035665eec497
Kiids <1980774293@qq.com>
2022-11-16 01:28:40 +0800

    动态规划

--- 区间子数组个数.cpp
+++ 区间子数组个数.cpp
@@ -19,16 +19,16 @@ public:
         int lastR = -1, lastD = 0, ret = 0;
         for (int i = 0; i < nums.size(); i++)
         {
-            if (nums[i] > right)
+            if (nums[i] > right)      // 不满足条件重新计数 
             {
                 lastR = i;
                 lastD = 0;
             }
-            else if (nums[i] < left)
+            else if (nums[i] < left)  // 计算 ret 个数 
                 ret += lastD;
             else
             {
-                lastD = i - lastR;
+                lastD = i - lastR;    // 长度即包含 i 的子数组个数
                 ret += lastD;
             }
         }
@@ -36,13 +36,13 @@ public:
     }
 };
 
-//动态规划
-//d[i]表示包含i的情况下满足条件的子数组的个数
-//初始化d[i]都是0
-//nums[i] > right: 不满足，那么d[i]=0,并且记录lastR为上一个大于R的的序号
-//nums[i] < left，它不能成为单独满足，所以它等于 d[i-1]
-//nums[i] 在范围内，它的长度就是 i- lastR，也就是包含i的子数组的个数
-//结果sum_overi (d[i])
-//优化
-//只依赖于 lastR，LastD 和 累计的数组个数
+// 动态规划
+// d[i]表示包含i的情况下满足条件的子数组的个数
+// 初始化d[i]都是0
+// nums[i] > right: 不满足，那么d[i]=0,并且记录lastR为上一个大于R的的序号
+// nums[i] < left，它不能成为单独满足，所以它等于 d[i-1]
+// nums[i] 在范围内，它的长度就是 i- lastR，也就是包含i的子数组的个数
+// 结果sum_overi (d[i])
+// 优化
+// 只依赖于 lastR，LastD 和 累计的数组个数
 
