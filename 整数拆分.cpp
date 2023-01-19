30f29d9ccc76bf25f405b4bc917a5f2e66b8071a
Kiids <1980774293@qq.com>
2023-01-20 02:13:42 +0800

    动态规划

--- 整数拆分.cpp
+++ 整数拆分.cpp
@@ -29,3 +29,23 @@ public:
         return dp[n];
     }
 };
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1);
+        dp[2] = 1;
+        for(int i = 3; i <= n; i++)
+            for(int j = 1; j < i - 1; j++)
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+        return dp[n];
+    }
+};
+
+// 设dp[i]为分解i能得到的最大积
+// 遍历每一种分解方式，通过循环获得所有分解方式的最大值
+// dp[i] = max(dp[i], max(j*(i-j), j*dp[i-j]));
+// i从3到n循环一次，一层层往上得到dp[n]
+// 内层循环，条件为j < i-1，是因为j = i-1时对dp[1]分解毫无意义
+// 题目要求k >= 2，1无法分解为两个正整数相加
+
