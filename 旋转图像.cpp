f5666429049a401ec7be5eee7c9e516451a140f7
Kiids <1980774293@qq.com>
2023-01-30 00:46:39 +0800

    先上下调转，再以斜对角线为轴对称

--- 旋转图像.cpp
+++ 旋转图像.cpp
@@ -24,7 +24,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for (int i = 0; i < n / 2;i++)
+        for (int i = 0; i < n / 2; i++)
         {
             for (int j = 0; j < (n + 1) / 2; j++)
             {
@@ -35,5 +35,17 @@ public:
                 matrix[j][n - 1 - i] = t;
             }
         }
+    }
+};
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; i++)
+            matrix[i].swap(matrix[n - 1 - i]);
+        for (int i = 0; i < n; i++)
+            for (int j = i; j < n; j++)
+                swap(matrix[i][j], matrix[j][i]);
     }
 };
