eb639a75176ccb2d137eb772f37682cb2cb23179
Kiids <1980774293@qq.com>
2023-02-19 01:27:37 +0800

    哈希

--- 四数相加 II.cpp
+++ 四数相加 II.cpp
@@ -18,15 +18,22 @@ D = [ 0, 2]
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int, int> m;
-        for (int a : nums1)
+        unordered_map<int, int> m;   // key:nums1+nums2的数值，value:nums1+nums2数值出现的次数
+        for (int a : nums1)          // 遍历nums1和nums2数组，统计两个数组元素之和，和出现的次数，放到map中
             for (int b : nums2)
                 m[a + b]++;
-        int count = 0; 
-        for (int c : nums3)
+        int count = 0;               // 统计nums1+nums2+nums3+nums4=0 出现的次数
+        for (int c : nums3)          // 在遍历nums3和nums4数组，找到如果 0-(nums3+nums4) 在map中出现过的话，就把map中key对应的value也就是出现次数统计出来。
             for (int d : nums4)
                 if (m.find(0 - (c + d)) != m.end())
                     count += m[0 - (c + d)];
         return count;
     }
 };
+
+// 定义一个unordered_map，key放nums1和nums2两数之和，value 放nums1和nums2两数之和出现的次数。
+// 遍历nums1和nums2数组，统计两个数组元素之和，和出现的次数，放到map中。
+// count用来统计nums1+nums2+nums3+nums4=0出现的次数。
+// 遍历nums3和nums4数组，找到如果 0-(nums3+nums4) 在map中出现过的话，就用count把map中key对应的value也就是出现次数统计出来。
+// 最后返回统计值count
+
