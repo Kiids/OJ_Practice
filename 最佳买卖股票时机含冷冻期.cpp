df78372d3ec4b8308cca1e1453ad2f12a3da0e1a
Kiids <1980774293@qq.com>
2023-04-05 19:26:13 +0800

    动态规划

--- 最佳买卖股票时机含冷冻期.cpp
+++ 最佳买卖股票时机含冷冻期.cpp
@@ -33,8 +33,26 @@ public:
 // dp[i][0] 第i天持有股票，昨天就持有且不卖出 or 今天买入 
 // dp[i][1] 第i天卖出股票 
 // dp[i][2] 第i天不持有股票
-//由状态机得状态转移方程
-//dp[i][0]=max(dp[i-1][0],dp[i-1][2]-prices[i]);
-//dp[i][1]=dp[i-1][0]+prices[i];
-//dp[i][2]=max(dp[i-1][1],dp[i-1][2]);
+// 由状态机得状态转移方程
+// dp[i][0]=max(dp[i-1][0],dp[i-1][2]-prices[i]);
+// dp[i][1]=dp[i-1][0]+prices[i];
+// dp[i][2]=max(dp[i-1][1],dp[i-1][2]);
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0 || n == 1)
+            return 0;
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < n; i++)
+        {
+            dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][1], dp[i - 1][3]) - prices[i]);  // 拥有股票的状态
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);  // 没有买入时，没有股票的状态
+            dp[i][2] = dp[i - 1][0] + prices[i];  // 买入后，当天卖后没有股票的状态
+            dp[i][3] = dp[i - 1][2];  // 卖后进入冻结期
+        }
+        return max(dp[n - 1][3], max(dp[n - 1][2], dp[n - 1][1]));  // 最后比较三个没有股票状态的最大值
+    }
+};
