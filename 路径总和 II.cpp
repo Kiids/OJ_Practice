cbadf8a4fdf7f20038376f7ce0805de809b50c47
Kiids <1980774293@qq.com>
2023-04-24 00:04:50 +0800

    递归

--- 路径总和 II.cpp
+++ 路径总和 II.cpp
@@ -50,3 +50,44 @@ public:
         return v;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<vector<int>> v;
+    vector<int> path;
+    void DFS(TreeNode *root, int sum)
+    {
+        path.push_back(root->val);
+        sum -= root->val;
+        if (!root->left && !root->right)
+        {
+            if (!sum)
+                v.push_back(path);
+        }
+        else
+        {
+            if (root->left)
+                DFS(root->left, sum);
+            if (root->right)
+                DFS(root->right, sum);
+        }        
+        path.pop_back();
+    }
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if (!root)
+            return v;
+        DFS(root, targetSum);
+        return v;
+    }
+};
