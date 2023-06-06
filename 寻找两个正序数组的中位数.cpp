babf909961db0f8df79332a485c1f885bca02a83
Kiids <1980774293@qq.com>
2023-06-07 01:48:30 +0800

    二分查找

--- 寻找两个正序数组的中位数.cpp
+++ 寻找两个正序数组的中位数.cpp
@@ -73,3 +73,22 @@ public:
 // 当指针移动的次数与 (m + n) / 2 相同时，得到中位数
 
 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size(), l = 0, r = m;
+        if (m > n)
+            return findMedianSortedArrays(nums2, nums1);
+        while (l < r)
+        {
+            int i = (l + r) / 2, j = (m + n + 1) / 2 - i;
+            nums1[i] < nums2[j - 1] ? l = ++i : r = i;
+        }
+        r = (m + n + 1) / 2 - l;
+        int subL = max(l <= 0 ? INT_MIN : nums1[l - 1], r <= 0 ? INT_MIN : nums2[r - 1]);
+        if ((m + n) & 1)
+            return subL;
+        int subR = min(l >= m ? INT_MAX : nums1[l], r >= n ? INT_MAX : nums2[r]);
+        return (subL + subR) / 2.0;
+    }
+};
