c53b503fefc622c0f8ad8e692b86ea64d937a1c1
Kiids <1980774293@qq.com>
2023-06-15 02:09:05 +0800

    快速排序

--- 数组中的第K个最大元素.cpp
+++ 数组中的第K个最大元素.cpp
@@ -1,6 +1,7 @@
 /*
 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
 示例 1:
 输入: [3,2,1,5,6,4] 和 k = 2
 输出: 5
@@ -30,3 +31,42 @@ public:
         return q.top();
     }
 };
+
+
+class Solution {
+    int partition(vector<int>& v, int l, int r)
+    {
+        int x = v[l], t = l;
+        while (l < r)
+        {
+            while (l < r && v[r] <= x)
+                r--;
+            while (l < r && v[l] >= x)
+                l++;
+            if (l < r)
+                swap(v[l], v[r]);
+        }
+        swap(v[t], v[l]);
+        return l;
+    }
+
+    int fun(int k, vector<int>& v, int l, int r)
+    {
+        while (true)
+        {
+            int idx = partition(v, l, r);
+            if (idx == k - 1)
+                return v[idx];
+            else if (idx < k - 1)
+                l = idx + 1;
+            else    
+                r = idx - 1;
+        }
+    }
+
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return fun(k, nums, 0, nums.size() - 1);
+    }
+};
+// 快速排序  时间复杂度 O(n)
