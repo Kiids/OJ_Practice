ee17f9ad5208d69eb133c4df504e167df0829125
Kiids <1980774293@qq.com>
2023-06-30 03:41:46 +0800

    双链表&哈希表

--- LRU 缓存.cpp
+++ LRU 缓存.cpp
@@ -1,9 +1,9 @@
 /*
 请你设计并实现一个满足  LRU (最近最少使用) 缓存 约束的数据结构。
 实现 LRUCache 类：
-LRUCache(int capacity) 以 正整数 作为容量?capacity 初始化 LRU 缓存
+LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
 int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
-void put(int key, int value)?如果关键字?key 已经存在，则变更其数据值?value ；如果不存在，则向缓存中插入该组?key-value 。如果插入操作导致关键字数量超过?capacity ，则应该 逐出 最久未使用的关键字。
+void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
 示例：
 输入
@@ -35,10 +35,10 @@ public:
         int key, value;
         Node *left ,*right;
         Node(int _key,int _value): key(_key), value(_value), left(NULL), right(NULL) {}
-    }*L,*R;
+    }*L,*R;  // 双链表节点 
 
     int n;
-    unordered_map<int, Node*> hash;
+    unordered_map<int, Node*> hash;  // 哈希 
 
     void remove(Node* p)
     {
@@ -82,7 +82,7 @@ public:
         {
             if (hash.size() == n)
             {
-                auto  p = R->left;
+                auto p = R->left;
                 remove(p);
                 hash.erase(p->key);
                 delete p;
